749ace8f59a73dd9aedc7dd59bd93520bc046201
6310301009 <71169517+Phumai9@users.noreply.github.com>
2023-08-23 14:55:32 +0700

    Update 1-queue.py

--- class8-asyncio/1-queue.py
+++ class8-asyncio/1-queue.py
@@ -46,3 +46,18 @@ async def main():
 
 # start the asyncio program
 asyncio.run(main())
+
+#Wed Aug 23 14:50:20 2023 Producer: Running
+#Wed Aug 23 14:50:20 2023 Consumer: Running
+#Wed Aug 23 14:50:21 2023 >got 0.6749485067820122
+#Wed Aug 23 14:50:21 2023 >got 0.584160924671123
+#Wed Aug 23 14:50:22 2023 >got 0.9565908530460696
+#Wed Aug 23 14:50:23 2023 >got 0.18521223709512646
+#Wed Aug 23 14:50:23 2023 >got 0.3367991038165442
+#Wed Aug 23 14:50:23 2023 >got 0.37651114058578206
+#Wed Aug 23 14:50:24 2023 >got 0.8884777545808717
+#Wed Aug 23 14:50:24 2023 >got 0.1771594501511916
+#Wed Aug 23 14:50:25 2023 >got 0.5885975988041554
+#Wed Aug 23 14:50:26 2023 Producer: Done
+#Wed Aug 23 14:50:26 2023 >got 0.6497389921716039
+#Wed Aug 23 14:50:26 2023 Consumer: Done
